4dcdff51c86f2e13af5279f53b5d4caf0be7f15b
Ngo Khoi <ngokhoivn@gmail.com>
2025-07-06 09:37:23 +0700

    Bai_82

---
+++ Source/Crunch/Private/Widgets/AbilityGauge.h
@@ -0,0 +1,29 @@
+﻿//AbilityGauge.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityGauge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAbilityGauge : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UImage* Icon;               // Hiển thị icon kỹ năng
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* CooldownCounterText; // Đếm ngược thời gian hồi chiêu
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CooldownDurationText; // Hiển thị tổng thời gian hồi chiêu
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CostText;       // Hiển thị chi phí (năng lượng, mana...)
+};
