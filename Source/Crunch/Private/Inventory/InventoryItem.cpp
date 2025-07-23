89f558f51f99a50889e964530ab4c476df85b6a2
Ngo Khoi <ngokhoivn@gmail.com>
2025-07-23 08:00:35 +0700

    Bai_136

--- Source/Crunch/Private/Inventory/InventoryItem.cpp
+++ Source/Crunch/Private/Inventory/InventoryItem.cpp
@@ -3,3 +3,49 @@
 
 #include "Inventory/InventoryItem.h"
 
+FInventoryItemHandle::FInventoryItemHandle()
+    : HandleId{GetInvalidId()}
+{
+}
+
+FInventoryItemHandle FInventoryItemHandle::InvalidHandle()
+{
+    static FInventoryItemHandle InvalidHandle = FInventoryItemHandle();
+    return InvalidHandle;
+}
+
+FInventoryItemHandle::FInventoryItemHandle(uint32 Id)
+    : HandleId{Id}
+{
+}
+
+bool FInventoryItemHandle::IsValid() const
+{
+    return HandleId != GetInvalidId();
+}
+
+FInventoryItemHandle FInventoryItemHandle::CreateHandle()
+{
+    return FInventoryItemHandle(GenerateNextId());
+}
+
+uint32 FInventoryItemHandle::GenerateNextId()
+{
+    static uint32 StaticId = 1;
+    return StaticId++;
+}
+
+uint32 FInventoryItemHandle::GetInvalidId()
+{
+    return 0;
+}
+
+bool operator==(const FInventoryItemHandle& Lhs, const FInventoryItemHandle& Rhs)
+{
+    return Lhs.GetHandleId() == Rhs.GetHandleId();
+}
+
+uint32 GetTypeHash(const FInventoryItemHandle& Key)
+{
+    return Key.GetHandleId();
+}
--- Source/Crunch/Private/Inventory/InventoryItem.h
+++ Source/Crunch/Private/Inventory/InventoryItem.h
@@ -6,6 +6,29 @@
 #include "UObject/NoExportTypes.h"
 #include "InventoryItem.generated.h"
 
+USTRUCT()
+struct FInventoryItemHandle
+{
+	GENERATED_BODY()
+public:
+	FInventoryItemHandle();
+	static FInventoryItemHandle InvalidHandle();
+	static FInventoryItemHandle CreateHandle();
+
+	bool IsValid() const;
+	uint32 GetHandleId() const { return HandleId; }
+private:
+	explicit FInventoryItemHandle(uint32 Id);
+
+	UPROPERTY()
+	uint32 HandleId;
+
+	static uint32 GenerateNextId();
+	static uint32 GetInvalidId();
+};
+
+bool operator==(const FInventoryItemHandle& Lhs,const FInventoryItemHandle& Rhs);
+uint32 GetTypeHash(const FInventoryItemHandle& Key);
 /**
  * 
  */
